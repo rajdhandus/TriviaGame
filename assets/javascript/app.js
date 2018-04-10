dd20110ef84b9b57aa7ee350e707b5b56ecd9efa
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2018-04-09 23:33:31 -0400

    Checking out for today.. zzZZZZ

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,19 @@
 function getRandomQA() {
-    
+    var randNum = Math.floor(Math.random() * 2);
+    return questions[randNum];
+}
+
+function showQuestion(obj) {
+    $("#questions").text(obj.question);
+    $("#choice-1").text(obj.choice1);
+    $("#choice-2").text(obj.choice2);
+    $("#choice-3").text(obj.choice3);
+    $("#choice-4").text(obj.choice4);
+}
+
+function main() {
+    var qObj = getRandomQA();
+    showQuestion(qObj);
 }
 
 var questions = [
