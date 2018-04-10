065d69f2036cc6a64aba512733185f45764c3908
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2018-04-09 23:21:18 -0400

    Base skeleton framework...

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+function getRandomQA() {
+    
+}
+
+var questions = [
+    {
+        question : "What is your favorite color?",
+        choice1 : "red",
+        choice2 : "blue",
+        choice3 : "green",
+        choice4 : "yello",
+        correctAnswer : 1
+    },{
+        question : "What is your favorite game?",
+        choice1 : "baseball",
+        choice2 : "football",
+        choice3 : "soccer",
+        choice4 : "cricket",
+        correctAnswer : 4
+    }
+]
